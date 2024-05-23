967ad029d35a3b31f3b217b7c49e31c55c03dd9a
lonGDiBo <longpm211@gmail.com>
2024-05-23 18:25:04 +0700

    code eda

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import *
 from markupsafe import escape
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
@@ -15,54 +17,53 @@ def load_data(file_path):
     data = pd.read_csv(file_path)
     return data
 
-def eda_stocks(dataset,column_name):
-    data = dataset.copy()
-    data['Date'] = pd.to_datetime(data['Date'])
-    data.set_index('Date', inplace=True)
-    plt.clf() 
-    data[column_name].plot(color = 'blue')
-    plt.title('Close Price Over Time')
-    plt.xlabel('Date')
-    plt.ylabel('Close Price')
-    plt.savefig('static/images/plot.png')  # Save the plot as an image
-    plt.close()
-    
-    
     
 @app.route('/')
 def index():
     return render_template('login.html')
 
+global_data = None
+global_name = None
 
 @app.route('/data', methods=['GET', 'POST'])
 def data():
     data_loaded = False
     stock_name = None
-    column_name = None
     data = None
     columns = None
-
+    file_path= None
+    global global_data
+    global global_name
     if request.method == 'POST':
         if 'data' in request.form:
             stock_name = request.form['data']
             file_path = f"{stock_name}.csv"
-            data = load_data(f"./data/{file_path}")
-            data = data.head(10)
-            data_loaded = True
+            data = load_data(f"./dataset/{file_path}")
+            global_data = data.copy()
+            global_name = stock_name
+            data_loaded = True   
             columns = data.columns.tolist()
+    return render_template('main.html', data=data.to_html() if data is not None else None, data_loaded=data_loaded,columns = columns,stock_name=stock_name)
 
+@app.route('/eda_column', methods=['GET', 'POST'])
+def eda_data():
+    global global_data  
+    global global_name
+    data_1 = global_data.copy()
+    column_name = None
+    if request.method == 'POST':
         if 'column' in request.form:
             column_name = request.form.get('column')
-
-    return render_template('main.html', data=data.to_html() if data is not None else None, columns=columns, data_loaded=data_loaded, stock_name=stock_name, column_name=column_name)
-
-
-@app.route('/get_column_data', methods=['POST'])
-def get_column_data():
-    stock_name = request.form['stock_name']
-    column_name = request.form['column_name']
-    file_path = f"./data/{stock_name}.csv"
-    data = load_data(file_path)
-    eda_stocks(data, column_name)
-    return redirect(url_for('data'))
-    
+            plt.figure(figsize=(10, 5))
+            if global_name == 'GOOGLE' or global_name == 'APPLE' or global_name == 'AMAZON':
+                data_1['Date'] = pd.to_datetime(data_1['Date'])
+                plt.plot(data_1['Date'],data_1[column_name])
+            elif global_name == 'Weather_WS':
+                data_1['Date Time'] = pd.to_datetime(data_1['Date Time'])
+                plt.plot(data_1['Date Time'],data_1[column_name])
+            elif global_name == 'weather-HCM':
+                data_1['date'] = pd.to_datetime(data_1['date'])
+                plt.plot(data_1['date'],data_1[column_name])
+            plt.title(column_name)
+            plt.savefig('static/images/plot.png')
+    return jsonify(column_name=column_name)
